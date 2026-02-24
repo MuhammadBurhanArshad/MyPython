0e417aad83b449200fc8c12d692ea492bc67381f
Muhammad Burhan Arshad <112951994+MuhammadBurhanArshad@users.noreply.github.com>
2026-02-24 15:09:06 +0500

    Add multiple utility functions for various checks

--- Practice/LabTwo.py
+++ Practice/LabTwo.py
@@ -65,3 +65,118 @@ def addTwoNum(a, b):
     print(c)
     
 addTwoNum(2, 3)
+
+
+
+
+def check_number(x):
+    if(x < 0):
+        return "Negative"
+    elif(x > 0):
+        return "Positive"
+    else:
+        return "Zero"
+        
+print(check_number(0))
+
+
+
+
+
+def check_even_odd(n):
+    if(n % 2 == 0):
+        return "Even"
+    else:
+        return "Odd"
+
+print(check_even_odd(1))
+
+
+
+
+def get_age_category(age):
+    if age < 13:
+        return "Child"
+    elif 13 < age >= 19:
+        return "Teenager"
+    elif 20 < age >= 59:
+        return "Adult"
+    else:
+        age >= 60
+        return "Senior"
+
+        
+print(get_age_category(10))
+
+
+
+
+
+def compare_numbers(a, b):
+    if(a > b):
+        return "First is Greater"
+    elif(b > a):
+        return "Second is Greater"
+    else:
+        return "Both are Equal"
+    
+print(compare_numbers(2,3))
+
+
+
+#5
+
+
+def validate_login(username, password):
+    correct_username = "admin"
+    correct_password = "1234"
+    
+    if username != correct_username:
+        return "Invalid Username"
+    elif password != correct_password:
+        return "Invalid Password"
+    else:
+        return "Login Successful"
+    
+print(validate_login("admin", "1234"))
+
+
+
+
+
+#6
+
+
+
+def check_temperature(temp):
+    if temp <= 0:
+        return "Freezing"
+    elif 1 <= temp <= 15:
+        return "Cold"
+    elif 16 <= temp <= 30:
+        return "Warm"
+    else:
+        return "Hot"
+
+    
+print(check_temperature(-5))
+
+
+
+# 7
+
+
+
+def calculate_shipping(order_amount):
+    if order_amount < 1000:
+        shipping = 200
+    elif 1000 <= order_amount <= 4999:
+        shipping = 100
+    else:
+        shipping = 0
+    
+    total = order_amount + shipping
+    return f"Total Payable Amount: {total} (Shipping: {shipping})"
+
+
+print(calculate_shipping(800))
